b0c30010381fb17f583785b9764019707d4cc13e
Devanshu78 <devanshup.pandey281@gmail.com>
2021-10-01 00:38:37 +0530

    demo only

--- home - Copy/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from home import views
-
-urlpatterns = [
-    path("", views.index, name='home'),
-    path('submit/' , views.submit , name = 'submit')
-]
